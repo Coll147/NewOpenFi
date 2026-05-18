d57682102513fce327826965458e2bb6a7dc6a11
Coll147 <91793263+Coll147@users.noreply.github.com>
2026-05-18 08:46:10 +0200

    add more pages to the dashboard

--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -1,9 +1,36 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.render('pages/dashboard/main', {
+    title: 'OpenFi - Dashboard'
+  });
+});
+
 router.get('/devices', (req, res) => {
   res.render('pages/dashboard/devices', {
     title: 'OpenFi - Devices'
+  });
+});
+
+
+router.get('/logs', (req, res) => {
+  res.render('pages/dashboard/logs', {
+    title: 'OpenFi - Logs'
+  });
+});
+
+
+router.get('/wifi', (req, res) => {
+  res.render('pages/dashboard/wifi', {
+    title: 'OpenFi - WiFi'
+  });
+});
+
+
+router.get('/networks', (req, res) => {
+  res.render('pages/dashboard/networks', {
+    title: 'OpenFi - Networks'
   });
 });
 
